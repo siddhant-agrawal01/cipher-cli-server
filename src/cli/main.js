0fc19e58d181699b68a2439ea7c327d55a5881d1
siddhant agrawal <siddhant-agrawal01>
2025-11-23 17:12:51 +0530

    made the exec file for cli

--- src/cli/main.js
+++ src/cli/main.js
@@ -13,12 +13,28 @@ dotenv.config();
 async function main() {
   //Display banner
   console.log(
-    chalk.green(
+    chalk.magenta(
       figlet.textSync("Cipher CLI", {
         font: "Standard",
         horizontalLayout: "default",
       })
     )
   );
-  console.log(chalk.blue("AI based Cli \n"));
+  console.log(chalk.gray("AI based Cli \n"));
+
+  const program = new Command("orbital");
+  program
+    .version("0.0.1")
+    .description("Cipher Cli - AI that runs your workflow");
+
+  program.action(() => {
+    program.help();
+  });
+
+  program.parse();
 }
+
+main().catch((err) => {
+  console.log(chalk.red("Some Error running in cipher cli"));
+  process.exit(1);
+});
