94039cbb07a4032b413fb04ba0bdab29896a98ba
siddhant agrawal <siddhant-agrawal01>
2025-11-23 18:37:32 +0530

    setup the cli with chalk

---
+++ src/cli/commands/auth/login.js
@@ -0,0 +1,20 @@
+import { cancel, confirm, intro, isCancel, outro  } from "@clack/prompts";
+import { logger } from "better-auth";
+import { createAuthClient } from "better-auth/client";
+import { deviceAuthorizationClient } from "better-auth/client/plugins";
+import chalk from "chalk";
+import { Command } from "commander";
+import fs from "fs/promises";
+import open from "open";
+import os from "os";
+import path from "path";
+import yoctoSpinner from "yocto-spinner";
+import * as z from "zod/v4";
+import dotenv from "dotenv";
+import prisma from "../../../lib/db.js";
+
+dotenv.config()
+
+const URL = "http://localhost:5000";
+
+const 
